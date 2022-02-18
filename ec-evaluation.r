23250c2dd2b1d41dda7bcb4eef4748805bf1f704
Christoph Häni <christoph.haeni@bfh.ch>
2022-02-18 10:11:39 +0100

    Fix frequency log scale

--- ec-evaluation.r
+++ ec-evaluation.r
@@ -6,7 +6,7 @@
 ## 16.01.2018 - Marcel - Funktion zur Berechnung der Pfadlänge GF hinzugefügt
 # *************************************************************************************
 
-REddy.version <- "v20190905"
+REddy.version <- "v20220218"
 library(tcltk)
 library(xlsx)
 library(lubridate)
@@ -652,17 +652,17 @@ plot_cospec_ogive <- function(ogive,cospec,freq,ylab=NULL,xlim=NULL,cx=1.5,col="
 	if(is.null(xlim))xlim <- rev(range(freq))
 	ylim <- c(min(ogive,0),max(0,max(ogive)))
 	pxlim <- pretty(log10(xlim),n=ceiling(abs(diff(log10(xlim)))))
-	pxlims <- rep(pxlim,each=9) - log10(seq(9, 1))
+	pxlims <- rep(pxlim,each=8) + log10(seq(2, 9)) - 1
 	pylim <- pretty(ylim)
 	prCo <- pretty(rCo) 
 	y_cf <- (cospec_f - min(prCo))/diff(range(prCo))*diff(ylim) + ylim[1]
 	y_crm <- (cospec_rm - min(prCo))/diff(range(prCo))*diff(ylim) + ylim[1]
 	py2 <- (prCo - min(prCo))/diff(range(prCo))*diff(ylim) + ylim[1]
 	
-	plot(1,xlim=xlim,ylim=ylim, cex.axis=cx, cex.lab=cx,type="n",log="x",xaxt="n",yaxt="n",xlab="log of frequency [Hz]",ylab="",panel.first=abline(h=0,col=col,lty=2))
+	plot(1,xlim=xlim,ylim=ylim, cex.axis=cx, cex.lab=cx,type="n",log="x",xaxt="n",yaxt="n",xlab="frequency [Hz]",ylab="",panel.first=abline(h=0,col=col,lty=2))
 	abline(h=(0 - min(prCo))/diff(range(prCo))*diff(ylim) + ylim[1],lty=2,col="darkgrey")
 	axis(1,at=10^pxlims,labels=FALSE,tck=-0.01, cex.axis=cx, cex.lab=cx)
-	axis(1,at=10^pxlim,labels=pxlim, cex.axis=cx, cex.lab=cx)
+	axis(1,at=10^pxlim,labels=10 ^ pxlim, cex.axis=cx, cex.lab=cx)
 	axis(2,at=pylim,labels=pylim, cex.axis=cx, cex.lab=cx, col=col,col.axis=col,lwd=2,font=2)
 	title(ylab=ylab,col.lab=col, cex.lab=cx,font.lab=2)
 	axis(4,at=py2,labels=prCo, cex.axis=cx, cex.lab=cx)
@@ -683,13 +683,13 @@ plot_damping <- function(ogive_damp,freq,ylab=NULL,xlim=NULL,ylim=NULL,cx=1.5,cx
 	if(is.null(xlim))xlim <- rev(range(freq))
 	if(is.null(ylim))ylim <- c(min(0,ogive,ogive_ref_pbreg,ogive_ref_deming),max(0,max(ogive,ogive_ref_pbreg,ogive_ref_deming)))
 	pxlim <- pretty(log10(xlim),n=ceiling(abs(diff(log10(xlim)))))
-	pxlims <- rep(pxlim,each=9) - log10(seq(9, 1))
+	pxlims <- rep(pxlim,each=8) + log10(seq(2, 9)) - 1
 	pylim <- pretty(ylim)
 
-	plot(1,xlim=xlim,ylim=ylim,main=main, cex.axis=cx, cex.lab=cx,type="n",log="x",xaxt="n",xlab="log of frequency [Hz]",ylab=ylab,panel.first=abline(h=0,col=col,lty=2))
+	plot(1,xlim=xlim,ylim=ylim,main=main, cex.axis=cx, cex.lab=cx,type="n",log="x",xaxt="n",xlab="frequency [Hz]",ylab=ylab,panel.first=abline(h=0,col=col,lty=2))
 	abline(h=0,lty=2,col="darkgrey")
 	axis(1,at=10^pxlims,labels=FALSE,tck=-0.01, cex.axis=cx, cex.lab=cx)
-	axis(1,at=10^pxlim,labels=pxlim, cex.axis=cx, cex.lab=cx)
+	axis(1,at=10^pxlim,labels=10 ^ pxlim, cex.axis=cx, cex.lab=cx)
 	axis(3,at=1/c(0.02,0.05,0.1,1,10,60,120,300,600,1800,3600,7200),labels=c("20ms","50ms","100ms","1s","10s","1min","2min","5min","10min","30min","1hr","2hrs"), cex.axis=cx, cex.lab=cx)
 	lines(freq,ogive_ref_deming,col="darkgrey",lwd=2)
 	lines(freq,ogive_ref_pbreg,col="lightgrey",lwd=2)

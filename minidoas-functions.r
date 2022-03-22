f7ad28f9a9bbc72f6d00aef442ef7f4b49eb034d
Christoph Häni <christoph.haeni@bfh.ch>
2022-03-22 10:01:33 +0100

    Use local scripts if path available

--- minidoas-functions.r
+++ minidoas-functions.r
@@ -1,8 +1,6 @@
 ### ******************************************************************************
 ### ******************************************************************************
 ### ******************* functions for miniDOAS data evaluation *******************
-### *************************** author: Joerg Sintermann *************************
-### this works with the Avaspec and QE65-pro from Jan.2014 on, given converted raw-files by python script
 ### ******************************************************************************
 ### ******************************************************************************
 
@@ -17,17 +15,22 @@ require(MASS)
 # options(shiny.trace=FALSE)
 
 
-# source("~/repos/5_GitHub/gel-scripts/minidoas/miniDOAS-functions-filters.r")
-# source("~/repos/5_GitHub/gel-scripts/minidoas/miniDOAS-functions-fitting.r")
-# source("~/repos/5_GitHub/gel-scripts/minidoas/miniDOAS-functions-plotting.r")
-# source("~/repos/5_GitHub/gel-scripts/minidoas/miniDOAS-functions-spectrometer.r")
-# source("~/repos/5_GitHub/gel-scripts/minidoas/miniDOAS-functions-others.r")
-# source("~/repos/5_GitHub/gel-scripts/minidoas/miniDOAS-functions-tools.r")
-devtools::source_url("https://github.com/ChHaeni/gel-scripts/blob/main/minidoas/miniDOAS-functions-filters.r?raw=TRUE")
-devtools::source_url("https://github.com/ChHaeni/gel-scripts/blob/main/minidoas/miniDOAS-functions-fitting.r?raw=TRUE")
-devtools::source_url("https://github.com/ChHaeni/gel-scripts/blob/main/minidoas/miniDOAS-functions-plotting.r?raw=TRUE")
-devtools::source_url("https://github.com/ChHaeni/gel-scripts/blob/main/minidoas/miniDOAS-functions-spectrometer.r?raw=TRUE")
-devtools::source_url("https://github.com/ChHaeni/gel-scripts/blob/main/minidoas/miniDOAS-functions-others.r?raw=TRUE")
-devtools::source_url("https://github.com/ChHaeni/gel-scripts/blob/main/minidoas/miniDOAS-functions-tools.r?raw=TRUE")
+if (dir.exists('~/repos/5_GitHub/gel-scripts')) {
+    # local
+    source("~/repos/5_GitHub/gel-scripts/minidoas/miniDOAS-functions-filters.r")
+    source("~/repos/5_GitHub/gel-scripts/minidoas/miniDOAS-functions-fitting.r")
+    source("~/repos/5_GitHub/gel-scripts/minidoas/miniDOAS-functions-plotting.r")
+    source("~/repos/5_GitHub/gel-scripts/minidoas/miniDOAS-functions-spectrometer.r")
+    source("~/repos/5_GitHub/gel-scripts/minidoas/miniDOAS-functions-others.r")
+    source("~/repos/5_GitHub/gel-scripts/minidoas/miniDOAS-functions-tools.r")
+} else {
+    # remote
+    devtools::source_url("https://github.com/ChHaeni/gel-scripts/blob/main/minidoas/miniDOAS-functions-filters.r?raw=TRUE")
+    devtools::source_url("https://github.com/ChHaeni/gel-scripts/blob/main/minidoas/miniDOAS-functions-fitting.r?raw=TRUE")
+    devtools::source_url("https://github.com/ChHaeni/gel-scripts/blob/main/minidoas/miniDOAS-functions-plotting.r?raw=TRUE")
+    devtools::source_url("https://github.com/ChHaeni/gel-scripts/blob/main/minidoas/miniDOAS-functions-spectrometer.r?raw=TRUE")
+    devtools::source_url("https://github.com/ChHaeni/gel-scripts/blob/main/minidoas/miniDOAS-functions-others.r?raw=TRUE")
+    devtools::source_url("https://github.com/ChHaeni/gel-scripts/blob/main/minidoas/miniDOAS-functions-tools.r?raw=TRUE")
+}
 
 cat("\n*** Skript", programVersion, "***\n")

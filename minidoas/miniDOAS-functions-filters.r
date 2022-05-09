da591a69b610ccfb597fc7c25dc3f69dcfb321a2
Christoph Häni <christoph.haeni@bfh.ch>
2022-05-09 11:27:14 +0200

    Replace old filter.function list

--- minidoas/miniDOAS-functions-filters.r
+++ minidoas/miniDOAS-functions-filters.r
@@ -60,12 +60,12 @@ options(
 
 # highpass filter function
 highpass.filter <- function(dat, DOAS.win, ...){
-    filt <- filter.functions[[DOAS.win$filter.type]](DOAS.win$filter.strength, ...)
+    filt <- getOption('md.filter.function.list')[[DOAS.win$filter.type]](DOAS.win$filter.strength, ...)
     dat[DOAS.win$pixel_filter] - filter(dat[DOAS.win$pixel_filter], filt, 'convolution', 2, circular = FALSE)
 }
 
 # complementary lowpass filter function
 lowpass.filter <- function(dat, filter.type = 'Rect', filter.strength = 5, ...){
-    filt <- filter.functions[[filter.type]](filter.strength, ...)
+    filt <- getOption('md.filter.function.list')[[filter.type]](filter.strength, ...)
     filter(dat, filt, 'convolution', 2, circular = FALSE)
 }

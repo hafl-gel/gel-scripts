7af0fe357c7a7c37fe5545dfd23c68aeb2b093e9
Christoph Häni <christoph.haeni@bfh.ch>
2023-07-11 14:53:46 +0200

    Start xnode script

---
+++ xnode-functions.r
@@ -0,0 +1,14 @@
+
+# json format
+# 1 file per minute
+# data structure of folders: year/mont/day
+# file name contains date_time of recording
+
+# select time range -> get relevant files
+
+# read files
+
+# reshape data
+
+# convert to ibts?
+

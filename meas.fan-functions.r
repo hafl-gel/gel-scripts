a6632880fb813ec8e3d7aefc05f925e384c40656
Christoph Häni <christoph.haeni@bfh.ch>
2023-11-17 14:32:55 +0100

    [meas.fan] scrape manual for fan calibrations

--- meas.fan-functions.r
+++ meas.fan-functions.r
@@ -1,5 +1,7 @@
 
 # NOTES:
+# file paths:
+# 'messventilator-630mm-1/frequi-1_63_2023_11_14.csv'
 
 # TODO:
 
@@ -11,7 +13,7 @@
 
 path_lfe <- '~/LFE/02_Daten/9-Messventilator'
 fan_id <- '1'
-path_prep <- file.path(path_lfe, 'messventilator-%s0mm-%s/frequi-%s_%s_%i_%i_%i.csv')
+path_prep <- file.path(path_lfe, 'messventilator-%s0mm-%s')
 
 # get diameter
 fan_dia <- switch(as.character(fan_id)
@@ -24,5 +26,33 @@ fan_dia <- switch(as.character(fan_id)
 )
 
 # create path
-path_fan
-'/messventilator-630mm-1/frequi-1_63_2023_11_14.csv'
+path_fan <- sprintf(path_prep, fan_dia, fan_id)
+
+## 3. Calibration data ----------------------------------------
+
+##  • scrape manual ====================
+
+if (FALSE) {
+    library(tabulizer)
+    path_manual <- '~/LFE/02_Daten/9-Messventilator/AQC-HZ-G-B-AL00006.pdf'
+    all_tabs <- extract_tables(file = path_manual)
+    # str(all_tabs)
+    tabs_mat <- cbind(all_tabs[[11]][, 8], all_tabs[[12]][, 4:5])
+    cfs <- apply(tabs_mat, 2, function(x) {
+        tab <- apply(do.call(rbind, strsplit(
+                sub(',', '.', x[-(1:4)])
+                    , split = ' ')), 2, as.numeric)
+        x11()
+        plot(tab[, 2], tab[, 1])
+        m <- lm(tab[, 1] ~ tab[, 2])
+        abline(m)
+        setNames(list(coef(m)), x[[1]])
+    })
+    saveRDS(cfs, '~/repos/5_GitHub/gel-scripts/meas.fan/fan-calibration.rds')
+}
+    
+
+##  • 630 mm ====================
+
+hz_63 <- c(
+    4.01 = 24

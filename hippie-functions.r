ea25f295a5ff0f266fc757e764aec8f7db325597
Christoph Häni <christoph.haeni@bfh.ch>
2024-03-15 17:38:07 +0100

    [hippie] add first version of vial raw data processing

--- hippie-functions.r
+++ hippie-functions.r
@@ -66,6 +66,41 @@ read_hippie <- function(file, as_ibts = TRUE) {
     structure(lo_res, hi_res = hi_res)
 }
 
+
+read_vials <- function(file) {
+    require(openxlsx)
+    dat <- read.xlsx(file, sep.names = ' ')
+    # get column names
+    nms <- names(dat)
+    # find concentration columns
+    conc_cols <- grep('NH4.*meas', nms, value = TRUE)
+    # find related times
+    time_cols <- grep('time.*meas', nms, value = TRUE)
+    # find start
+    start_col <- grep('analysis.*start', nms, value = TRUE)
+    # fix start time
+    dat[, start_col] <- parse_date_time3(dat[[start_col]])
+    # find Cycle/Position/etc.
+    cycle_cols <- grep('(C|c)ycle|(P|p)osition|(C|c)hannel|(R|r)ank', nms, value = TRUE)
+    # find net ml
+    net_col <- grep('net.*m(l|L)', nms, value = TRUE)
+    # find vial nr.
+    vial_col <- grep('vial.*nr', nms, value = TRUE)
+    # build output
+    out <- dat[, c(vial_col, cycle_cols, net_col, conc_cols, start_col, time_cols)]
+    # add mg in Solution and time difference
+    for (i in seq_along(conc_cols)) {
+        # total NH4-N
+        out[, paste0('nh4.n.tot.mg', i)] <- out[, conc_cols[i]] * out[, net_col] / 1000
+        # reaction time
+        out[, paste0('reaction.time', i)] <- parse_date_time3(out[[time_cols[i]]]) - out[, start_col]
+    }
+    # fix names
+    names(out) <- make.names(tolower(names(out)))
+}
+file <- '~/LFE/02_Daten/6-hippie/lab/Vials_blau_41-80_20240221.xlsx'
+read_vials(file)
+
 # test_file <- '~/LFE/02_Daten/6-hippie/20240215.TXT'
 # read_hippie(test_file)
 

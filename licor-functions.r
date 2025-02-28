4f6cbd44a615ef2d4e11bd1eff27e376739f020c
Christoph Häni <christoph.haeni@bfh.ch>
2025-02-28 13:26:26 +0100

    [licor] give up on faster version

--- licor-functions.r
+++ licor-functions.r
@@ -12,10 +12,10 @@ columns <- c(1, 3, 5:14)
 col_classes <- c('POSIXct', 'character', 'numeric', 'character', 
     rep('numeric', 10), 'character', 'character')
 
-f <- '~/LFE/08_gelhub/wauwilermoos/licor/py_fnf_01_licor_2025_02_19.csv'
-# f <- '~/LFE/08_gelhub/wauwilermoos/licor/py_fnf_01_licor_2025_02_20.csv'
+# f <- '~/LFE/08_gelhub/wauwilermoos/licor/py_fnf_01_licor_2025_02_19.csv'
+f <- '~/LFE/08_gelhub/wauwilermoos/licor/py_fnf_01_licor_2025_02_20.csv'
 # f <- '~/LFE/08_gelhub/wauwilermoos/licor/py_fnf_01_licor_2025_02_24.csv'
-rm(xx, yy)
+rm(xx, yy, bzz, czz, dzz)
 for (i in 1:10) gc()
 system.time({
     xx <- readLines(f)
@@ -23,100 +23,27 @@ system.time({
     bzz <- na.omit(fread(text = yy, select = columns, col.names = col_names, 
         colClasses = col_classes, fill = TRUE))
 })
-rm(xx, yy)
-for (i in 1:10) gc()
-system.time({
-    xx <- fread(f, sep = ',')
-    dzz <- na.omit(xx[, c(list(V1), 
-        fread(text = gsub('[)]{,2}[(][^ ]+ ', ',', V2),
-            select = columns[-1], col.names = col_names[-1], 
-            colClasses = c('character', col_classes[-1]), fill = TRUE)
-        )])
-})
-rm(xx, yy)
+rm(xx, yy, bzz)
 for (i in 1:10) gc()
 system.time({
     czz <- read_licor_data(f)
 })
+rm(czz)
 
 as.data.table(licor_read_cpp(normalizePath(f)))
 
 f_test <- 'test'
 uu <- licor_read_cpp(normalizePath(f_test))
-as.data.table(uu)
+head(as.data.table(uu), n = 8)
 
-cppFunction('
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <Rcpp.h>
-Rcpp::List licor_read_cpp(String filename) {
-    // open file
-    std::ifstream finput{filename};
-    if (!finput.is_open()) {
-        Rcout << "Could not read file: " << filename.get_cstring() << "\\n";
-        return R_NilValue;
-    }
-    // create output
-    int max_lines = 870000;
-    std::vector<std::string> col1_time(max_lines);
-    std::vector<int> col4_DiagVal(max_lines);
-    std::vector<double> col6_CO2D(max_lines);
-    std::vector<double> col7_H2OD(max_lines);
-    std::vector<double> col8_Temp(max_lines);
-    std::vector<double> col9_Pres(max_lines);
-    std::vector<double> col10_Cooler(max_lines);
-    std::vector<double> col11_SFVin(max_lines);
-    std::vector<double> col12_H2OMF(max_lines);
-    std::vector<double> col13_DewPt(max_lines);
-    std::vector<double> col14_CO2SS(max_lines);
-    std::vector<int> col15_CO2AWO(max_lines);
-    int cline = 0;
-    int n_fields = 17 - 1;
-    int field = 0;
-    std::vector<std::string> line(n_fields + 1);
-    // loop over lines
-    char c;
-    bool append = true;
-    // create field chars + dummy
-    char s[256];
-    while (finput.read(s, 14) && cline < 2) {
-        // fixed read time (14 chars) -> check comma
-        // check comma -> otherwise read to eol
-        finput.get(c);
-        if (c != \',\') {
-        // erroneous line
-            // check s for eol
-            // if eol -> rewind => loop finput.putpack()
-            // else getline until eol
-            continue;
-        }
-        // terminate string
-        s[14] = \'\\0\';
-        col1_time[cline] = s;
-        // fixed skip up to DiagVal -> check space
-        // fixed read DiagVal -> check closing parenthesis
-        // etc.
-        // fixed read eol (newline or carriage return)
-        // increment line
-        cline++;
-    }
-    return Rcpp::List::create(
-		_["time_string"] = col1_time,
-        _["DiagVal"] = col4_DiagVal,
-        _["CO2D"] = col6_CO2D,
-        _["H2OD"] = col7_H2OD,
-        _["Temp"] = col8_Temp,
-        _["Pres"] = col9_Pres,
-        _["Cooler"] = col10_Cooler,
-        _["SFVin"] = col11_SFVin,
-        _["H2OMF"] = col12_H2OMF,
-        _["DewPt"] = col13_DewPt,
-        _["CO2SS"] = col14_CO2SS,
-        _["CO2AWO"] = col15_CO2AWO
-    );
-}
-')
+aa <- readLines(f_test, n = 8)
+
+
+for (i in 1:10) gc()
+f <- '~/LFE/08_gelhub/wauwilermoos/sonic/py_fnf_01_sonic_2025_02_20.csv'
+system.time({
+    czz <- fread(f, fill = TRUE)
+})
 
 cppFunction('
 #include <iostream>
@@ -242,3 +169,4 @@ read_licor_data <- function(FilePath) {
     # remove NA entries
     na.omit(out)
 }
+

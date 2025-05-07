f0f800b30d83ee0a46784f55f9aa9ee58851cd09
Christoph Häni <christoph.haeni@bfh.ch>
2025-05-07 14:22:54 +0200

    [meas.fan] adapt serialized string to vector

--- meas.fan-functions.r
+++ meas.fan-functions.r
@@ -125,23 +125,23 @@ read_frequi <- function(path_data, from = NULL, to = NULL, time_zone = 'UTC',
 
 ## 2. Calibration data ----------------------------------------
 
+### get data to script
 dat2string <- function(x) {
     key <- keygen(as.raw(rep(1, 32)))
     nonce <- as.raw(rep(1, 24))
     xs <- qs2::qd_serialize(x)
     xr <- data_encrypt(xs, key, nonce = nonce)
-    paste0(xr, collapse = '')
+    as.character(xr)
 }
 string2dat <- function(x) {
     key <- keygen(as.raw(rep(1, 32)))
     nonce <- as.raw(rep(1, 24))
-    xs <- strsplit(x, split = '(?<=.)(?!(..)*.$)', perl = TRUE)
-    xr <- as.raw(paste0('0x', unlist(xs)))
+    xr <- as.raw(paste0('0x', x))
     qs2::qd_deserialize(data_decrypt(xr, key, nonce = nonce))
 }
-splitN <- function(x, n) {
-    unlist(strsplit(x, 
-            split = paste0('(?<=.{', n, '})(?!(.{', n, '})*.$)'), perl = TRUE))
+pasteN <- function(x, n) {
+    y <- rbind(x, rep(c(rep('","', n - 1), '",\n"'), length(x))[1:length(x)])
+    paste0('c("', paste(y[-length(y)], collapse = ''), '")')
 }
 add_to_eof <- function(x, attr_name, header, f_name, ec_file) {
     con <- file(ec_file, open = 'a')
@@ -149,8 +149,8 @@ add_to_eof <- function(x, attr_name, header, f_name, ec_file) {
     on.exit({close(con)})
     writeLines(paste0('# ', header), con)
     writeLines(paste0(
-            'attr(', f_name, ', "', attr_name, '") <- string2dat(paste0(c(\n"',
-            paste(splitN(xs, 70), collapse = '",\n"'), '"), collapse = ""))\n'), con)
+            'attr(', f_name, ', "', attr_name, '") <- string2dat(\n',
+            pasteN(xs, 30), ')\n'), con)
 }
 
 ##  • fan calibration ====================
@@ -197,11 +197,12 @@ if (FALSE) {
 ## 4. Calibration Results ----------------------------------------
 
 # ~~~ fan coefficients ~~~
-attr(read_frequi, "fan_coefficients") <- string2dat(paste0(c(
-"1c15782abf437a97048e915491c9e4cff9f65c9cabef3907dd7f3e396a07fd6f882ef0",
-"2b49c0c9ec66aaffb1553c091642ea808ddfd186351b7c57b47dc770f5552241652de2",
-"008ec3bfab4d77ec3ba6a31493f8db91fb807b6bbde7e1e337316d90f0643beb1dc5a6",
-"3055a7490353625ab49c45a9eecf9536a7ff4c21c99920c456e83d39a66d867f53f945",
-"01fcc303d5d21d5a138e45057e7654e8c800592b5528a52e389dc76497e6c997f83442",
-"d90aad76776493e6"), collapse = ""))
+attr(read_frequi, "fan_coefficients") <- string2dat(
+c("1c","15","78","2a","bf","43","7a","97","04","8e","91","54","91","c9","e4","cf","f9","f6","5c","9c","ab","ef","39","07","dd","7f","3e","39","6a","07",
+"fd","6f","88","2e","f0","2b","49","c0","c9","ec","66","aa","ff","b1","55","3c","09","16","42","ea","80","8d","df","d1","86","35","1b","7c","57","b4",
+"7d","c7","70","f5","55","22","41","65","2d","e2","00","8e","c3","bf","ab","4d","77","ec","3b","a6","a3","14","93","f8","db","91","fb","80","7b","6b",
+"bd","e7","e1","e3","37","31","6d","90","f0","64","3b","eb","1d","c5","a6","30","55","a7","49","03","53","62","5a","b4","9c","45","a9","ee","cf","95",
+"36","a7","ff","4c","21","c9","99","20","c4","56","e8","3d","39","a6","6d","86","7f","53","f9","45","01","fc","c3","03","d5","d2","1d","5a","13","8e",
+"45","05","7e","76","54","e8","c8","00","59","2b","55","28","a5","2e","38","9d","c7","64","97","e6","c9","97","f8","34","42","d9","0a","ad","76","77",
+"64","93","e6"))
 

fa2dc22b2cddee5c5537de2045981fc269ce130f
Christoph Häni <christoph.haeni@bfh.ch>
2025-06-26 13:02:18 +0200

    Add option to provide data directory and time range for licor

--- R/licor-functions.r
+++ R/licor-functions.r
@@ -1,32 +1,82 @@
 
 # R wrapper, main function
-read_licor <- function(file_path) {
-	# get file name
-	bn <- basename(file_path)
-    # check file name
-    if (grepl('[.]qdata$', bn)) {
-        return(alloc.col(qs2::qd_read(file_path)))
-    } else if (grepl('[.]qs$', bn)) {
-        if (!requireNamespace('qs')) {
-            stop('data is provided as *.qs file -> install qs library',
-                ' running "install.packages("qs")"')
+read_licor <- function(file_path, from = NULL, to = NULL, tz = 'UTC') {
+    # parse from/to
+    from <- parse_date_time3(from, tz = tz)
+    to <- parse_date_time3(to, tz = tz)
+    # check if directory is provided
+    if (file.info(file_path)$isdir) {
+        files <- dir(file_path)
+        if (length(files) == 0L) {
+            stop('directory is empty!')
         }
-        return(alloc.col(qs::qread(file_path)))
-    } else if (grepl('[.]rds$', bn)) {
-        return(readRDS(file_path))
-    }
-    if (grepl('[.]gz$', bn)) {
-        raw_list <- licor_read_cpp_gzip(normalizePath(file_path))
+        # check dates in filenames
+        suppressWarnings(
+            file_dates <- as.integer(
+                sub('.*(20[2-9]\\d)_?(\\d{2})_?(\\d{2}).*', '\\1\\2\\3', files)
+            )
+        )
+        # check from & to
+        read_me <- rep(TRUE, length(file_dates))
+        if (length(from) > 0) {
+            read_me <- read_me & file_dates >= as.integer(
+                        format(floor_date(from, unit = 'days'), '%Y%m%d')
+                        )
+        }
+        if (length(to) > 0) {
+            read_me <- read_me & file_dates <= as.integer(
+                        format(floor_date(to, unit = 'days'), '%Y%m%d')
+                        )
+        }
+        # if no dates => read
+        read_me <- read_me | is.na(read_me)
+        # loop over files
+        if (any(read_me)) {
+            out <- lapply(file.path(file_path, files[read_me]), read_licor,
+                from = from, to = to, tz = tz)
+            # return sorted
+            rbindlist(out)[order(Time)]
+        } else {
+            stop('No files available within provided time range!')
+        }
     } else {
-        raw_list <- licor_read_cpp(normalizePath(file_path))
+        # be verbose
+        cat("File:", path.expand(file_path), "- ")
+        # get file name
+        bn <- basename(file_path)
+        # check file name
+        if (grepl('[.]qdata$', bn)) {
+            return(alloc.col(qs2::qd_read(file_path)))
+        } else if (grepl('[.]qs$', bn)) {
+            if (!requireNamespace('qs')) {
+                stop('data is provided as *.qs file -> install qs library',
+                    ' running "install.packages("qs")"')
+            }
+            return(alloc.col(qs::qread(file_path)))
+        } else if (grepl('[.]rds$', bn)) {
+            return(readRDS(file_path))
+        }
+        if (grepl('[.]gz$', bn)) {
+            raw_list <- licor_read_cpp_gzip(normalizePath(file_path))
+        } else {
+            raw_list <- licor_read_cpp(normalizePath(file_path))
+        }
+        out <- data.table::as.data.table(raw_list)
+        # convert time
+        out[, Time := fast_strptime(time_string, '%Y-%m-%dT%H:%M:%OSZ', lt = FALSE)]
+        # remove time_string
+        out[, time_string := NULL]
+        # filter by from/to
+        if (length(from) > 0) {
+            out <- out[Time >= from]
+        }
+        if (length(to) > 0) {
+            out <- out[Time <= to]
+        }
+        # place Time first
+        setcolorder(out, 'Time')
+        cat('done\n')
+        # remove NA entries
+        na.omit(out)
     }
-    out <- data.table::as.data.table(raw_list)
-    # convert time
-    out[, Time := fast_strptime(time_string, '%Y-%m-%dT%H:%M:%OSZ', lt = FALSE)]
-    # remove time_string
-    out[, time_string := NULL]
-    # place Time first
-    setcolorder(out, 'Time')
-    # remove NA entries
-    na.omit(out)
 }

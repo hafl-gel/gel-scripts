bbe66dbe07c5553a56f98e285570156cb043723c
Christoph Häni <christoph.haeni@bfh.ch>
2026-05-07 14:07:03 +0200

    [sonic] fix further sonic label issues

--- R/read-sonic-data.r
+++ R/read-sonic-data.r
@@ -152,11 +152,14 @@ read_windmaster_ascii <- function(file_path){
         return(NULL)
     }
     # be verbose and print sonic names:
-    sonic_label <- out[, gsub('^[^A-Z]*([A-Z])?.*', '\\1', 
+    sonic_label <- out[, gsub('^[^A-D]*([A-D])?.*', '\\1', 
         iconv(unique(V2), 'UTF-8', 'UTF-8', sub = '')
     )]
     sonic_label <- unique(sonic_label[!(sonic_label == '')])
-    if (length(sonic_label) > 1) stop('more than one unique sonic label!')
+    if (length(sonic_label) > 1) {
+        stop('more than one unique sonic label: ', paste(
+            paste0('"', sonic_label, '"'), collapse = ' & '))
+    }
     cat(paste0("data recorded by sonic-", tolower(sonic_label), "\n"))
     if (!(loggerbox <- grepl('^py_fnf_0', bn))) {
         sonic_file <- sub("data_(.*)_[0-9]{8}_[0-9]{6}([.]gz)?$", "\\1", bn)

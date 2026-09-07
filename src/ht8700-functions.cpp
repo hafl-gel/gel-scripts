ac8482c46f1b3914c4f62cf3fbd2fc4bee86b74f
Christoph Häni <christoph.haeni@bfh.ch>
2026-09-07 15:05:42 +0200

    [ht] ht logic has to be different because we read all fields

--- src/ht8700-functions.cpp
+++ src/ht8700-functions.cpp
@@ -38,69 +38,63 @@ Rcpp::List ht8700_read_cpp(String filename) {
     NumericVector col15(max_lines, NA_REAL);
     NumericVector col16(max_lines, NA_REAL);
     int cline = 0;
-    int n_fields = 20; // we're not interested in the fields after field with index 19
+    int n_fields = 20; // read full 20 fields
     int field = 0;
     std::vector<std::string> line(n_fields);
     // loop over lines
     char c;
     std::string s;
     while (input.get(c)) {
-        if (field < n_fields) {
-            if (c == ',') {
-                // comma => increment field
+        if (c == ',') {
+            // comma => increment field
+            // add s to current line vector
+            line[field] = s;
+            // increase field counter
+            field += 1;
+            // reset s
+            s.clear();
+        } else if (c == '\n') {
+            if (field == (n_fields - 1)) {
+                // newline => line ok (field 20 ended with newline)
                 // add s to current line vector
                 line[field] = s;
-                // increase field counter
-                field += 1;
-                // reset s
-                s.clear();
-            } else if (c == '\n') {
-                // premature newline
-                // reset field counter
-                field = 0;
-                // reset s
-                s.clear();
-                // increase line counter
-                cline += 1;
-            } else {
-                // append to string or new line
-                s += c;
+                // assign to vectors
+                col1_time[cline] = line[0];
+                try {
+                    col2[cline] = std::stoi(line[1]);
+                } catch (...) {
+                    col2[cline] = NA_INTEGER;
+                }
+                col3[cline] = std::stod(line[2]);
+                col4[cline] = std::stod(line[3]);
+                col5[cline] = std::stod(line[4]);
+                col6[cline] = std::stod(line[5]);
+                col7[cline] = std::stod(line[6]);
+                col8[cline] = std::stod(line[7]);
+                col9[cline] = std::stod(line[8]);
+                col10[cline] = std::stod(line[9]);
+                col11[cline] = std::stod(line[10]);
+                col12[cline] = std::stod(line[11]);
+                col13[cline] = std::stod(line[12]);
+                col14[cline] = std::stod(line[13]);
+                col15[cline] = std::stod(line[14]);
+                col16[cline] = std::stod(line[15]);
+                col17[cline] = line[16];
+                col18[cline] = line[17];
+                col19[cline] = std::stoi(line[18]);
+                col20[cline] = line[19];
             }
-        } else if (c == '\n') {
-            // newline => line ok (field 20 ended with comma)
-            // assign to vectors
-            col1_time[cline] = line[0];
-            try {
-                col2[cline] = std::stoi(line[1]);
-            } catch (...) {
-                col2[cline] = NA_INTEGER;
-            }
-            col3[cline] = std::stod(line[2]);
-            col4[cline] = std::stod(line[3]);
-            col5[cline] = std::stod(line[4]);
-            col6[cline] = std::stod(line[5]);
-            col7[cline] = std::stod(line[6]);
-            col8[cline] = std::stod(line[7]);
-            col9[cline] = std::stod(line[8]);
-            col10[cline] = std::stod(line[9]);
-            col11[cline] = std::stod(line[10]);
-            col12[cline] = std::stod(line[11]);
-            col13[cline] = std::stod(line[12]);
-            col14[cline] = std::stod(line[13]);
-            col15[cline] = std::stod(line[14]);
-            col16[cline] = std::stod(line[15]);
-            col17[cline] = line[16];
-            col18[cline] = line[17];
-            col19[cline] = std::stoi(line[18]);
-            col20[cline] = line[19];
+            // else premature newline
             // reset field counter
             field = 0;
             // reset s
             s.clear();
             // increase line counter
             cline += 1;
+        } else {
+            // append to string or new line
+            s += c;
         }
-        // else ignore all characters up to newline
     }
     return Rcpp::List::create(
 		_["time_string"] = col1_time,
@@ -158,69 +152,63 @@ Rcpp::List ht8700_read_cpp_gzip(Rcpp::String filename) {
     NumericVector col15(max_lines, NA_REAL);
     NumericVector col16(max_lines, NA_REAL);
     int cline = 0;
-    int n_fields = 20; // we're not interested in the fields after field with index 19
+    int n_fields = 20; // read full 20 fields
     int field = 0;
     std::vector<std::string> line(n_fields);
     // loop over lines
     char c;
     std::string s;
     while (gzread(input, &c, 1) > 0) {
-        if (field < n_fields) {
-            if (c == ',') {
-                // comma => increment field
+        if (c == ',') {
+            // comma => increment field
+            // add s to current line vector
+            line[field] = s;
+            // increase field counter
+            field += 1;
+            // reset s
+            s.clear();
+        } else if (c == '\n') {
+            if (field == (n_fields - 1)) {
+                // newline => line ok (field 20 ended with newline)
                 // add s to current line vector
                 line[field] = s;
-                // increase field counter
-                field += 1;
-                // reset s
-                s.clear();
-            } else if (c == '\n') {
-                // premature newline
-                // reset field counter
-                field = 0;
-                // reset s
-                s.clear();
-                // increase line counter
-                cline += 1;
-            } else {
-                // append to string or new line
-                s += c;
+                // assign to vectors
+                col1_time[cline] = line[0];
+                try {
+                    col2[cline] = std::stoi(line[1]);
+                } catch (...) {
+                    col2[cline] = NA_INTEGER;
+                }
+                col3[cline] = std::stod(line[2]);
+                col4[cline] = std::stod(line[3]);
+                col5[cline] = std::stod(line[4]);
+                col6[cline] = std::stod(line[5]);
+                col7[cline] = std::stod(line[6]);
+                col8[cline] = std::stod(line[7]);
+                col9[cline] = std::stod(line[8]);
+                col10[cline] = std::stod(line[9]);
+                col11[cline] = std::stod(line[10]);
+                col12[cline] = std::stod(line[11]);
+                col13[cline] = std::stod(line[12]);
+                col14[cline] = std::stod(line[13]);
+                col15[cline] = std::stod(line[14]);
+                col16[cline] = std::stod(line[15]);
+                col17[cline] = line[16];
+                col18[cline] = line[17];
+                col19[cline] = std::stoi(line[18]);
+                col20[cline] = line[19];
             }
-        } else if (c == '\n') {
-            // newline => line ok (field 20 ended with comma)
-            // assign to vectors
-            col1_time[cline] = line[0];
-            try {
-                col2[cline] = std::stoi(line[1]);
-            } catch (...) {
-                col2[cline] = NA_INTEGER;
-            }
-            col3[cline] = std::stod(line[2]);
-            col4[cline] = std::stod(line[3]);
-            col5[cline] = std::stod(line[4]);
-            col6[cline] = std::stod(line[5]);
-            col7[cline] = std::stod(line[6]);
-            col8[cline] = std::stod(line[7]);
-            col9[cline] = std::stod(line[8]);
-            col10[cline] = std::stod(line[9]);
-            col11[cline] = std::stod(line[10]);
-            col12[cline] = std::stod(line[11]);
-            col13[cline] = std::stod(line[12]);
-            col14[cline] = std::stod(line[13]);
-            col15[cline] = std::stod(line[14]);
-            col16[cline] = std::stod(line[15]);
-            col17[cline] = line[16];
-            col18[cline] = line[17];
-            col19[cline] = std::stoi(line[18]);
-            col20[cline] = line[19];
+            // else premature newline
             // reset field counter
             field = 0;
             // reset s
             s.clear();
             // increase line counter
             cline += 1;
+        } else {
+            // append to string or new line
+            s += c;
         }
-        // else ignore all characters up to newline
     }
     // close properly
     if (gzclose(input) != Z_OK) {
